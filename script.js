ac8d80c38aed69a630c5628e2198de24b1a8ffc6
syed <71326007+SyedUmairAnwar@users.noreply.github.com>
2024-03-13 16:37:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 function mincost(arr)
 {
-	let sum=0;
- function rec(arr){
-	 if (arr.length>1){
-	 arr=arr.sort();
-	let  first=arr.shift();
-	let  second=arr.shift();
-	 sum += first+second;
-		 arr.push(first+second);
-	  rec(arr);
-	 }
-	
-
- }
-	rec(arr);
-	console.log(sum);
+  //write your code here
+  
+  arr.sort((a,b)=>a-b)
+   
+ let cost = 0
+    //console.log(arr)
+  
+  while(arr.length>1){
+       
+ let first = arr.shift()
+       
+ let second = arr.shift()
+       
+ cost+=first+second
+     
+   arr.push(first+second)
+      
+  arr.sort((a,b)=>a-b)
+      //  console.log(arr)
+    }
+  
+  return cost
 }
 module.exports=mincost;
