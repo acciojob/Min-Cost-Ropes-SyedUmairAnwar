e0f1db8e380ea5232a70665daa81ff367e0f8267
syed <71326007+SyedUmairAnwar@users.noreply.github.com>
2024-03-13 16:28:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+{
+	let sum=0;
+ function rec(arr){
+	 if (arr.length>1){
+	 arr=arr.sort();
+	 first=arr.shift();
+	 second=arr.shift();
+	 sum += first+second;
+		 arr.push(first+second);
+	  rec(arr);
+	 }
+	console.log(sum);
+	 return
+ }
 }
-
 module.exports=mincost;
